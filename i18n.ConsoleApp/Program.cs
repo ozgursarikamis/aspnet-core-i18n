63ac9a3b93aa2682e15261012e15de8f18b37b1b
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-12 23:47:44 +0300

    DisplayCurrentCulture

--- i18n.ConsoleApp/Program.cs
+++ i18n.ConsoleApp/Program.cs
@@ -7,9 +7,18 @@ namespace i18n.ConsoleApp
     {
         private static void Main()
         {
+            DisplayCurrentCulture();
             var en = new CultureInfo("tr-TR");
             CultureInfo.CurrentCulture = en;
             Console.WriteLine(CultureInfo.CurrentCulture);
+        }
+
+        private static void DisplayCurrentCulture()
+        {
+            Console.WriteLine("=============================");
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            Console.WriteLine(CultureInfo.CurrentCulture.DisplayName);
+            Console.WriteLine("=============================");
         }
     }
 }
