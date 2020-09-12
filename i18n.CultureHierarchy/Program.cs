63b5df6ce6a744e162eed8693ff30165f145ccd9
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 00:47:10 +0300

    regions, switch Current Culture to parent

--- i18n.CultureHierarchy/Program.cs
+++ i18n.CultureHierarchy/Program.cs
@@ -7,11 +7,17 @@ namespace i18n.CultureHierarchy
     {
         private static void Main()
         {
-            CultureInfo.CurrentCulture = new CultureInfo("en-US");
-            Console.WriteLine($"Current Culture: {CultureInfo.CurrentCulture}");
-            Console.WriteLine($"Is Neutral: {CultureInfo.CurrentCulture.IsNeutralCulture}");
-            Console.WriteLine($"Parent Culture: {CultureInfo.CurrentCulture.Parent}"); //ietf code
-            Console.WriteLine("============================================================");
+            CultureInfo.CurrentCulture = new CultureInfo("zh-Hant-HK");
+
+            for (var i = 0; i < 4; i++)
+            { 
+                Console.WriteLine($"Current Culture: {CultureInfo.CurrentCulture}");
+                Console.WriteLine($"Is Neutral: {CultureInfo.CurrentCulture.IsNeutralCulture}");
+                Console.WriteLine($"Parent Culture: {CultureInfo.CurrentCulture.Parent}"); //ietf code
+                Console.WriteLine("============================================================");
+
+                CultureInfo.CurrentCulture = CultureInfo.CurrentCulture.Parent;
+            }
         }
     }
 }
