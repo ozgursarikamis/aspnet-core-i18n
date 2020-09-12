f986eb2484b33eb78f0211dbfdb36660696adfe7
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 00:42:51 +0300

    Culture Hierarchy

---
+++ i18n.CultureHierarchy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace i18n.CultureHierarchy
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+            Console.WriteLine($"Current Culture: {CultureInfo.CurrentCulture}");
+            Console.WriteLine($"Is Neutral: {CultureInfo.CurrentCulture.IsNeutralCulture}");
+            Console.WriteLine($"Parent Culture: {CultureInfo.CurrentCulture.Parent}"); //ietf code
+            Console.WriteLine("============================================================");
+        }
+    }
+}
