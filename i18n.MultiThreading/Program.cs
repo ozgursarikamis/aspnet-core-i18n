424dddf12570a55fbbc980645fcccc3db4191e57
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 00:28:38 +0300

    ThreadInfo

--- i18n.MultiThreading/Program.cs
+++ i18n.MultiThreading/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace i18n.MultiThreading
 {
@@ -6,7 +9,31 @@ namespace i18n.MultiThreading
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ThreadInfo();
+
+            TaskFactory taskFactory = new TaskFactory();
+            taskFactory.StartNew(BackgroundTask);
+            Task.Delay(3000).Wait();
+
+            CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
+            ThreadInfo();
+            Task.Delay(3000).Wait();
+        }
+
+        private static void ThreadInfo()
+        {
+            Console.WriteLine("======================================= ThreadInfo");
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} : {CultureInfo.CurrentCulture.DisplayName}");
+            Console.WriteLine("======================================= ThreadInfo");
+        }
+
+        private static void BackgroundTask()
+        {
+            while (true)
+            {
+                ThreadInfo();
+                Task.Delay(1000).Wait();
+            }
         }
     }
 }
