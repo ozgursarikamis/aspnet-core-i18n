3dc3b1e45b398db894d36c1d7b05e3a82227b764
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 00:22:36 +0300

    MultiThreading project added

---
+++ i18n.MultiThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace i18n.MultiThreading
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
