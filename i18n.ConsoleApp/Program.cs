563507935a64d20affad9f82fd50c952a113631c
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-12 23:45:59 +0300

    change culture info

---
+++ i18n.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace i18n.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var en = new CultureInfo("tr-TR");
+            CultureInfo.CurrentCulture = en;
+            Console.WriteLine(CultureInfo.CurrentCulture);
+        }
+    }
+}
