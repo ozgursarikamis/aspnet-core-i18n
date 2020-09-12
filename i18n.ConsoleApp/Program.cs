8276b6e78e121f15fba49b2355895887b1f887ce
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-12 23:52:56 +0300

    DateTimeParseDemo

--- i18n.ConsoleApp/Program.cs
+++ i18n.ConsoleApp/Program.cs
@@ -9,24 +9,37 @@ namespace i18n.ConsoleApp
         {
             DisplayDateAndTime();
             DisplayCurrentCulture();
+
+            Console.WriteLine("============================= Change The Culture Info");
             var en = new CultureInfo("tr-TR");
             CultureInfo.CurrentCulture = en;
+
             DisplayDateAndTime();
 
             Console.WriteLine(CultureInfo.CurrentCulture);
+
+            DateTimeParseDemo();
         }
 
         private static void DisplayCurrentCulture()
         {
-            Console.WriteLine("=============================");
+            Console.WriteLine("============================= Display The Culture Info");
             Console.WriteLine(CultureInfo.CurrentCulture.Name);
-            Console.WriteLine(CultureInfo.CurrentCulture.DisplayName);
-            Console.WriteLine("=============================");
+            Console.WriteLine(CultureInfo.CurrentCulture.DisplayName); 
         }
 
         private static void DisplayDateAndTime()
         {
+            Console.WriteLine("============================= Display DateTime Info");
             Console.WriteLine(DateTime.Now);
+        }
+
+        private static void DateTimeParseDemo()
+        {
+            Console.WriteLine("============================= DateTime Parse");
+            const string dateString = "24.12.2016";
+            var date = DateTime.Parse(dateString);
+            Console.WriteLine(date.ToString("D"));
         }
     }
 }
