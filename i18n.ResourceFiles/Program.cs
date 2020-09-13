c47f5fc748763c0c81d73a6fed219d30c034d701
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 23:07:19 +0300

    using resource files

--- i18n.ResourceFiles/Program.cs
+++ i18n.ResourceFiles/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Resources;
+using i18n.ResourceFiles.Resources;
 
 namespace i18n.ResourceFiles
 {
@@ -6,7 +9,18 @@ namespace i18n.ResourceFiles
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine(HomePage.Welcome);
+
+            var manager = new ResourceManager(typeof(HomePage));
+
+            // option 1:
+            var welcomeMessage = manager.GetString("Welcome");
+            Console.WriteLine(welcomeMessage);
+
+            // option 2:
+            var ci = new CultureInfo("de-DE");
+            var welcomeMessageDe = manager.GetString("Welcome", ci);
+            Console.WriteLine(welcomeMessageDe);
         }
     }
 }
