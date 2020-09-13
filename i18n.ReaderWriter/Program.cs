500830f9e8d844d2ae31223d9ce76e4dd8186d7f
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 23:41:50 +0300

    generate resources runtime

--- i18n.ReaderWriter/Program.cs
+++ i18n.ReaderWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Resources;
 using System.Runtime.Loader;
 
@@ -8,24 +9,48 @@ namespace i18n.ReaderWriter
     {
         private static void Main()
         {
-            var path = AppContext.BaseDirectory + "\\de-DE\\i18n.ReaderWriter.resources.dll";
-            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
+            //var path = AppContext.BaseDirectory + "\\de-DE\\i18n.ReaderWriter.resources.dll";
+            //var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
 
-            var names = assembly.GetManifestResourceNames();
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
+            //var names = assembly.GetManifestResourceNames();
+            //foreach (var name in names)
+            //{
+            //    Console.WriteLine(name);
 
-                using var stream = assembly.GetManifestResourceStream(name);
-                using var resourceReader = new ResourceReader(stream ?? throw new InvalidOperationException());
+            //    using var stream = assembly.GetManifestResourceStream(name);
+            //    using var resourceReader = new ResourceReader(stream ?? throw new InvalidOperationException());
                 
-                var enumerator = resourceReader.GetEnumerator();
-                while (enumerator.MoveNext())
-                {
-                    Console.WriteLine($@"{enumerator.Key}: {enumerator.Value}");
-                }
+            //    var enumerator = resourceReader.GetEnumerator();
+            //    while (enumerator.MoveNext())
+            //    {
+            //        Console.WriteLine($@"{enumerator.Key}: {enumerator.Value}");
+            //    }
 
-                Console.WriteLine(@"------------------------------------------------------");
+            //    Console.WriteLine(@"------------------------------------------------------");
+            //}
+
+            var path = AppContext.BaseDirectory + "\\RobotCommands";
+            using (var stream = File.OpenWrite(path))
+            {
+                using (var resourceWriter = new ResourceWriter(stream))
+                {
+                    resourceWriter.AddResource("TL", "Turn Left");
+                    resourceWriter.AddResource("TR", "Turn right");
+                    resourceWriter.Generate();
+                }
+            }
+
+            using (var stream = File.OpenRead(path))
+            {
+                using (var resourceReader = new ResourceReader(stream))
+                {
+                    var enumerator = resourceReader.GetEnumerator();
+                    while (enumerator.MoveNext())
+                    {
+                        Console.WriteLine($@"{enumerator.Key}: {enumerator.Value}");
+                    }
+                    Console.WriteLine(@"------------------------------------------------------");
+                }
             }
         }
     }
