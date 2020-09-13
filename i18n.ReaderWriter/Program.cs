00358b21baff36fc477b92492e517c6154b7011e
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 23:28:15 +0300

    Reader & Writer demo added

---
+++ i18n.ReaderWriter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace i18n.ReaderWriter
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
