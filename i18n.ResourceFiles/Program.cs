b2bb78175e4a22e0fc691985de76f5132b4ae9ec
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-13 22:53:51 +0300

    Resource project added

---
+++ i18n.ResourceFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace i18n.ResourceFiles
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
