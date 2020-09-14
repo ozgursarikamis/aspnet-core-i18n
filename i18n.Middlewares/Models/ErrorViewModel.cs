b5ae6235cde483d599a66bfa02830d2cbea97caa
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-14 07:26:42 +0300

    localization project added

---
+++ i18n.Middlewares/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace i18n.Middlewares.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
