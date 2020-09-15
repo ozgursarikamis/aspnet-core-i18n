408c3a645a215923f122d8c981477b6736c5b160
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-15 10:18:12 +0300

    .

--- i18n.MvcImplementation/Controllers/HomeController.cs
+++ i18n.MvcImplementation/Controllers/HomeController.cs
@@ -1,18 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using i18n.MvcImplementation.Models;
 
 namespace i18n.MvcImplementation.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+    { 
         public IActionResult Index()
         {
             return View();
@@ -21,19 +13,12 @@ namespace i18n.MvcImplementation.Controllers
         [HttpPost]
         public IActionResult Contact(ContactModel model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            return View("ContactConfirmed", model.Comment);
+            return !ModelState.IsValid ? View(model) : View("ContactConfirmed", model.Comment);
         }
 
         public IActionResult Error()
         {
             return View();
-            ;
-            ;
         }
     }
 }
