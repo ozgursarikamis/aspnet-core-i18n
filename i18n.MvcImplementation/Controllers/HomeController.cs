dae8b0edd33a13dcc0f8eb2cff1531f88c9a46ae
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-15 10:27:32 +0300

    Contact view added

--- i18n.MvcImplementation/Controllers/HomeController.cs
+++ i18n.MvcImplementation/Controllers/HomeController.cs
@@ -5,15 +5,17 @@ namespace i18n.MvcImplementation.Controllers
 {
     public class HomeController : Controller
     { 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
         [HttpPost]
         public IActionResult Contact(ContactModel model)
         {
             return !ModelState.IsValid ? View(model) : View("ContactConfirmed", model.Comment);
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
         }
 
         public IActionResult Error()
